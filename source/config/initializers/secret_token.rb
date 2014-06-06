fadf11ff3c7bbe84d3e27d864867915e6fbc1cfb
Zach Karst <zach@karst.co>
2014-06-06 11:55:12 -0400

    Initial commit

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diggit::Application.config.secret_key_base = 'a7fbfb27b3240ed98ab782275a0c96016ced24c58d02713f59034abcf772db71e5d5f7b78a5d06ecabda8147f95bd3b1d274d04ff0bb781908d66aa80dfea227'
---
+++ source/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Diggit::Application.config.session_store :cookie_store, key: '_Diggit_session'
