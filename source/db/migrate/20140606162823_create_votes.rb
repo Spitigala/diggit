7a575cc09c5d43d257a4250c17abfd5a6279b21a
Zach Karst <zach@karst.co>
2014-06-06 16:40:50 -0400

    Create votes database migration
    Votes has:
    - votes_value (-1 value for downvote, +1 value to upvote)
    - polymorphic association (vote can belong to a song or a comment)

--- source/db/migrate/20140606162823_create_votes.rb
+++ source/db/migrate/20140606162823_create_votes.rb
@@ -1,6 +1,10 @@
 class CreateVotes < ActiveRecord::Migration
   def change
-    create_table :votes do |t|
+    create_table :votes do |t| do
+      t.integer :vote_value
+      t.voteable, polymorphic: true
+
+      t.timestamps
     end
   end
 end
