2751f4f59c87c39a7ad29eafe9c33735339de604
Zach Karst <zach@karst.co>
2014-06-06 15:54:44 -0400

    Add Users database migration
    User has:
    - name
    - email
    - password hash
    - bio
    - url of bio photo

--- source/db/migrate/20140606162720_create_users.rb
+++ source/db/migrate/20140606162720_create_users.rb
@@ -1,6 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :users do |t| do
+      t.string :name
+      t.string :email
+      t.string :password_hash
+      t.string :bio
+      t.string :photo_url
+
+      t.timestamps
     end
   end
 end
