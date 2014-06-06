c1a6ea284babac75ad3824a775be83d4decb031f
Zach Karst <zach@karst.co>
2014-06-06 17:26:23 -0400

    Add self referential association to comment model

--- source/app/models/comment.rb
+++ source/app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
-
   belongs_to :user
   has_many :votes, as: :voteable
 
+  has_many :replies, class_name: "Comment", foreign_key: "parent_id"
+
+  belongs_to :parent, class_name: "Comment"
 end
