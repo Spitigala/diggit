7f41ec50f16c5c0d43bf8e4dea4271a75f65bffc
Zach Karst <zach@karst.co>
2014-06-06 17:11:25 -0400

    Fix syntax error in create_votes migration file

--- source/db/migrate/20140606162823_create_votes.rb
+++ source/db/migrate/20140606162823_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :vote_value
-      t.voteable, polymorphic: true
+      t.references :voteable, polymorphic: true
 
       t.timestamps
     end
