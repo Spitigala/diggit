534bbb49784b0f1e8a32280d35256ffe8ceabc07
Tuan Duong <tuanduong15@gmail.com>
2014-06-06 22:35:22 -0400

    added Factory girl fixture to the application

--- source/config/application.rb
+++ source/config/application.rb
@@ -24,5 +24,16 @@ module Diggit
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+        g.test_framework :rspec,
+        fixtures: true,
+        view_specs: true,
+        helper_specs: false,
+        routing_spec: false,
+        controller_specs: true,
+        request_specs: false
+        g.fixture_placement :factory_girl, dir: "spec/factories"
+    end
   end
 end
