1b5a6b39adc7a7e8ca07f55298b9b0e563cd795f
Pei Liu <peixiong.liu@gmail.com>
2014-06-07 16:50:54 -0400

    Implemented basic routes and controller actions for songs

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -4,11 +4,25 @@ Diggit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'songs#index'
-  resources :songs do
-    resources :comments
+  resources :songs, except: [:edit, :update] do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+    resources :comments do
+      member do
+        post 'upvote'
+        post 'downvote'
+      end
+    end
   end
 
-  resources :users
+  resources :users, except: [:index, :edit, :update] do
+    member do
+      post 'login'
+      post 'logout'
+    end
+  end
 
   post '/users/login', to: 'users#login'
   post '/users/logout', to: 'users#logout'
