2768442963c232ab9b76100a643a10fa17607620
Pei Liu & "Donger <pair+girb0t.wontwon@devbootcamp.com>
2014-06-08 20:49:03 -0400

    Change album attribute to url for Song

--- source/spec/factories/song_factory.rb
+++ source/spec/factories/song_factory.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :song do
     title { Faker::Lorem.word }
     artist { Faker::Name.name }
-    album { Faker::Lorem.word }
+    url { Faker::Internet.url }
     association :uploader, factory: :user
 
     factory :invalid_song do
