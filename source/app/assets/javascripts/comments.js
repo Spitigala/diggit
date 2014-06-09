cb3ea66a4a31be096b94b4d9fd40f941dcf3383a
Pei Liu & "Donger <pair+girb0t.wontwon@devbootcamp.com>
2014-06-09 00:12:59 -0400

    User can now add and see comments to a song without refreshing page

--- source/app/assets/javascripts/comments.js
+++ source/app/assets/javascripts/comments.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
     $('.new_comment').submit(function(e){
       e.preventDefault();
       var comment = $('input[id=comment_content]').val()
-      var songId = $('input[id=comment_song_id').val()
+      var songID = $('input[id=comment_song_id').val()
       $.ajax({
         url: "/songs/" + songID + "/comments",
         type: "POST",
-        data: {comment: comment},
-        dataType: json
+        data: {comment: {content: comment}},
+        dataType: "json"
       }).done(function(response) {
-        console.log(response)
+        console.log(response);
+        var comment = '<li>Comment: ' + response.content + '<br>'
+        comment += 'Posted By: <a href="/users/' + response.user_id + '">' + response.username + '</a></li>'
+        $('ul').append(comment);
       })
     })
 
